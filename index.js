f5943847dbe055a26dc462c94592e77d6b980126
Maxwell Connor <mconnor142@gmail.com>
2019-08-25 03:04:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  var clone = Object.assign({}, object);
+  delete clone[key];
+  return clone;
+}
