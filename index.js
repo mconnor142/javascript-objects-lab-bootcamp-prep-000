788c34b8c06ae51b7e0ee99a9a022936fa942e12
Maxwell Connor <mconnor142@gmail.com>
2019-08-25 02:47:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = new Object({keys: '0'});
 
-function updateObjectWithKeyAndValue(object, key, value){
-  recipes[]
+function updateObjectWithKeyAndValue(recipes, key, value){
+  recipes[key] = value;
 }
